e1c185871b38e473fe71eedd8fd41344ea993102
Alex Pereverzev <a.o.pereverzev@gmail.com>
2014-12-10 15:37:25 +0200

    create lock activity

---
+++ app/src/main/java/com/pereverzev/encryptedimage/app/MainActivity.java
@@ -0,0 +1,16 @@
+package com.pereverzev.encryptedimage.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Александр on 10.12.2014.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
